c9c4f33845bb9ccd5ea58984eb19f1502ce2f91a
league <maddoxseul@sbsd.net>
2026-06-07 12:56:14 -0700

    finished first two :33

--- src/_01_algorithms/_2_fibonacci/fibo_natatata.java
+++ src/_01_algorithms/_2_fibonacci/fibo_natatata.java
@@ -7,19 +7,8 @@ public static void main(String[] args) {
 	int num2 = 1;
 	
 	for(int i=0; i<12;i++) {
-		System.out.print(String.valueOf(num1)+", "+String.valueOf(num2)+", ");
-		num1 += num2;
-		num2 += num1;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		System.out.print(String.valueOf(num1)+", "+String.valueOf(num2)+", "); 
+		num1 += num2; num2 += num1;
+	}	
 }
 }
